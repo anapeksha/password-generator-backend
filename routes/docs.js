5c9c8db29a351be68b4905aa802bd0c3802d953a
anapeksha <anapeksha.mukherjee@gmail.com>
2022-05-08 13:44:00 +0530

    trying custom css for swagger topbar

--- routes/docs.js
+++ routes/docs.js
@@ -8,7 +8,7 @@ const swaggerDocument = YAML.load("./docs/swagger.yml");
 
 //custom css for swagger
 var options = {
-	customCss: ".swagger-ui .topbar { display: none }",
+	customCss: ".swagger-ui .topbar { display: flex }",
 };
 
 router.use("/docs", swaggerUi.serve);
